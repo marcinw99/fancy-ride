ce24cab8a8a605579947b43c6420e2fc3dc47b51
Marcin Wąsik <marcinmw99@gmail.com>
2024-05-02 15:12:12 +0200

    feat: add prettier and prettify whole project, initial AppBar

---
+++ app/ui/AppBar/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export const AppBar = () => {
+  return (
+    <div>
+      <Image src="/FancyRide.png" alt="logo" width={203} height={37} />
+    </div>
+  );
+};
